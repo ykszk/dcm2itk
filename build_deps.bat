e3e37325a799657e654d8e29f9b1724b82ec4158
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2021-09-14 16:35:21 +0900

    Update build scripts for github actions

--- build_deps.bat
+++ build_deps.bat
@@ -2,7 +2,7 @@ SETLOCAL enabledelayedexpansion
 mkdir deps\build
 cd deps\build
 set G="Visual Studio 15 2017"
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110\bin\Hostx64\x64\cl.exe" set G="Visual Studio 16 2019"
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019" set G="Visual Studio 16 2019"
 cmake -G !G! -A x64 ..
 cmake --build . --config Release --parallel 3
 ENDLOCAL
