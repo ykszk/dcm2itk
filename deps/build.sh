bf68d39e14ee2672e88ad7ff79e5d2b142c3eb38
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2025-03-31 15:19:34 +0900

    Update minizip-ng to 3.0.10

--- deps/build.sh
+++ deps/build.sh
@@ -1,6 +1,6 @@
 mkdir -p build
 cd build
 cmake ..
-cmake --build . --config Release --parallel 4
+cmake --build . --config Release --parallel
 cmake -DZLIB_INCLUDE_DIR="$(pwd)/zlib-ng;$(pwd)/../zlib-ng" .. 
-cmake --build . --config Release --parallel 4
+cmake --build . --config Release --parallel
